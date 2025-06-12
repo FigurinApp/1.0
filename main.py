f0c5e4d215cc84f12b8c5f0773dfd9002ea8ce56
FigurinApp <thais.araujo52@hotmail.com>
2025-06-12 12:11:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory, render_template
+import os
+
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    file_path = os.path.join(app.static_folder, path)
+    if os.path.exists(file_path):
+        return send_from_directory(app.static_folder, path)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
