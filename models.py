8a2cd076f7ea0018f76987692e8156aea78591df
FigurinApp <thais.araujo52@hotmail.com>
2025-06-15 08:24:29 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,26 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    senha_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
 
-    def set_senha(self, senha):
-        self.senha_hash = bcrypt.generate_password_hash(senha).decode('utf-8')
+    def set_senha(self, senha_plana):
+        self.senha = generate_password_hash(senha_plana).decode('utf-8')
 
-    def check_senha(self, senha):
-        return bcrypt.check_password_hash(self.senha_hash, senha)
-
-class Figurinha(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    imagem_url = db.Column(db.String(255), nullable=False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    usuario = db.relationship('User', backref='figurinhas')
+    def check_senha(self, senha_plana):
+        return check_password_hash(self.senha, senha_plana)
